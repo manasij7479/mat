4006666ccd0a9bcf0b338d4c1c197db05054716c
Manasij Mukherjee <manasij7479@gmail.com>
2013-06-29 10:43:42 +0530

    fixed insertEdge

--- graph/adjlist.hpp
+++ graph/adjlist.hpp
@@ -17,18 +17,27 @@ namespace mat
 	public:
 		void insertVertex(const Vertex& v)
 		{
-			auto it = map.find(v);
-			if(it!=map.end())
+			if(map.find(v)==map.end())
 			{
-				*it = lists.size();
+				map[v] = lists.size();
 				lists.push_back({v,EdgeList()});
 			}
 		}
+		void removeVertex(const Vertex& v)
+		{
+			std::remove_if(begin(),end(),[&](VertexData& vd){return vd.v==v;});
+			for(auto& x = begin();x!=end();++x)
+			{
+				std::remove_if(nbegin(x->v),nend(x->v),[&](std::pair<Vertex,Edge> ve){return v==ve.first;});
+			}
+			
+		}
 		void insertEdge(const Vertex& x,const Vertex& y,const Edge& e)
 		{
-			EdgeList& list = lists[map[x]].list;
+			auto& list = getList(x);
 			auto p = std::make_pair(y,e);
 			auto it = std::find_if(list.begin(),list.end(),[&](std::pair<Vertex,Edge> v){return v.first==y;});
+			
 			if(it==list.end()||EdgePolicy::multi)
 				list.push_back(p);
 			else
@@ -36,15 +45,25 @@ namespace mat
 			if(!EdgePolicy::directed)
 				insertEdge(y,x,e);
 		}
+		void removeEdge(const Vertex& x,const Vertex& y,const Edge& e)
+		{
+			auto list = getList(x);
+			std::remove(list.first(),list.second(),std::make_pair(y,e));
+			if(!EdgePolicy::directed)
+				removeEdge(y,x,e);	
+		}
 		
 		typedef typename std::vector<VertexData>::iterator vertex_iterator;
 		vertex_iterator begin(){return lists.begin();}
 		vertex_iterator end(){return lists.end();}
 		
 		typedef typename EdgeList::iterator edge_iterator;
-		edge_iterator nbegin(const Vertex& v){return lists[map[v]].list.begin();}
-		edge_iterator nend(const Vertex& v){return lists[map[v]].list.end();}
+		edge_iterator nbegin(const Vertex& v){return getList(v).begin();}
+		edge_iterator nend(const Vertex& v){return getList(v).end();}
 	private:
+		
+		EdgeList& getList(const Vertex& v){return lists[map[v]].list;}
+		
 		std::vector<VertexData> lists; // The data for vertex v is stored in map[v] index of this list
 		std::unordered_map<Vertex,std::size_t> map;
 		
--- graph/test/test.cpp
+++ graph/test/test.cpp
@@ -2,6 +2,26 @@
 #include "../adjlist.hpp"
 int main()
 {
-	mat::AdjacencyList<int,bool> g;
+	mat::AdjacencyList<int,int> g;
+	g.insertVertex(3);
+	g.insertVertex(5);
+	g.insertVertex(2);
+	g.insertVertex(9);
 	
+	g.insertEdge(3,5,4);
+	g.insertEdge(2,9,3);
+	g.insertEdge(2,5,1);
+	g.insertEdge(2,3,8);
+	g.insertEdge(5,9,7);
+	
+	
+	for(auto x = g.begin();x!=g.end();++x)
+	{
+		std::cout<<"["<<x->v<<"]->";
+		for(auto y = g.nbegin(x->v);y!=g.nend(x->v);++y)
+		{
+			std::cout<<"("<< y->first<<","<< y->second<<")->";
+		}
+		std::cout<<"***"<<std::endl;
+	}
 }
