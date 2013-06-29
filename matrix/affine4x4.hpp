96ea78c05b0a17baab11b7099c0b969950cf8e19
Manasij Mukherjee <manasij7479@gmail.com>
2013-06-29 06:38:39 +0530

    split ops

---
+++ matrix/affine4x4.hpp
@@ -0,0 +1,10 @@
+#ifndef MAT_MATRIX_AFFINE4X4_HPP
+#define MAT_MATRIX_AFFINE4X4_HPP
+#include "matrix.hpp"
+namespace mat
+{
+	typedef Matrix<float,4,4,true> mat4;
+	typedef Matrix<double,4,4,true> dmat4;
+	
+}
+#endif
--- matrix/ops.hpp
+++ matrix/ops.hpp
@@ -3,34 +3,8 @@
 #include "matrix.hpp"
 namespace mat
 {
-	template<typename T,std::size_t P,std::size_t Q,std::size_t R,bool Order>
-	Matrix<T,P,R,Order> multiply(Matrix<T,P,Q,Order>& a,Matrix<T,Q,R,Order>& b)
-	{
-		Matrix<T,P,R,Order> m;
-		for(auto i=0;i<P;++i)
-		{
-			for(auto j=0;j<R;++j)
-			{
-				m[i][j]=0;
-				for(auto k=0;k<Q;++k)
-				{
-					m[i][j]+=a[i][k]*b[k][j];
-				}
-			}
-		}
-		return m;
-	}
-	
-	template<typename T,std::size_t R,std::size_t C,bool Order>
-	void transpose(Matrix<T,R,C,Order>& m)
-	{
-		for(auto i=0;i<R;++i)
-			for(auto j=0;i<C;++j)
-				std::swap(m[i][j],m[j][i]);
-	}
-
-	
-	
-	//TODO:Plenty of ops remaining..add and optimize as needed 
+	//TODO:Plenty of ops remaining..add and optimize as needed
 }
+#include "arithmetic.hpp"
+#include "affine4x4.hpp"
 #endif
