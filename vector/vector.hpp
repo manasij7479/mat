9c4f7aa91a5fd134c0c6263b214a86e3b882d868
ajax-crypto <ajax123crypto0@gmail.com>
2013-07-02 00:44:09 +0530

    New Commit
    Added few vector ops
    
    Signed-off-by: ajax-crypto <ajax123crypto0@gmail.com>

--- vector/vector.hpp
+++ vector/vector.hpp
@@ -8,8 +8,8 @@ namespace mat
 	template<typename T,std::size_t D> class Vector;
 }
 #endif
+
 #include "makevec.cxx" // Wide variety of factory functions
 #include "generic.cxx" // Generic definition
-#include "ops.hpp" // operations on vectors
 //#include "special.cxx" // Specializations, especially for float SIMD operations 
 
