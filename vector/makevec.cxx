ada634685622534c8e0730bafdaea2a893e1c5b1
Manasij Mukherjee <manasij7479@gmail.com>
2013-07-11 02:39:36 +0530

    removed va arg overload

--- vector/makevec.cxx
+++ vector/makevec.cxx
@@ -17,27 +17,28 @@ namespace mat
 		std::copy_n(begin,D,result.begin());
 		return result;
 	};
+
 	
 	/* GCC converts float to double for ... arguments. 
 	Function generates a runtime error.
 	Needs fix */ 
-	template
-	<
-		typename T = float,
-		std::size_t D = 4
-	>
-	Vector<T,D> make_vector(T t,...)
-	{
-		Vector<T,D> result;
-		unsigned int i=0;
-		result[i++]=t;
-		std::va_list vl;
-		va_start(vl,t);
-		while(i<D)
-			result[i++]=va_arg(vl,T);
-		va_end(vl);
-		return result;
-	}
+// 	template
+// 	<
+// 		typename T = float,
+// 		std::size_t D = 4
+// 	>
+// 	Vector<T,D> make_vector(T t,...)
+// 	{
+// 		Vector<T,D> result;
+// 		unsigned int i=0;
+// 		result[i++]=t;
+// 		std::va_list vl;
+// 		va_start(vl,t);
+// 		while(i<D)
+// 			result[i++]=va_arg(vl,T);
+// 		va_end(vl);
+// 		return result;
+// 	}
 
 	///TODO: Other Overloads
 }
