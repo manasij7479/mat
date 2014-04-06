b6064d1f81c0210eb12379733c823fde08d386b6
manasij7479 <manasij7479@gmail.com>
2014-04-07 05:23:05 +0530

    use unordered_map for parent array

--- graph/algorithm/test/test.cpp
+++ graph/algorithm/test/test.cpp
@@ -4,11 +4,14 @@
 #include <sstream>
 int main()
 {
-	std::string input("v 1\nv 2\nv 3\ne 1 2 1\ne 1 3 1\ne 2 3 1\nq");
+	std::string input("v 1\nv 2\nv 3\ne 1 2 1\ne 1 3 4\ne 2 3 1\nq");
 	std::istringstream in(input);
 	mat::AdjacencyList<int,int> g=mat::make_graph<int,int,mat::SimpleDigraph,mat::AdjacencyList>(in);
 	mat::debug_display(g,std::cout);
 	
-	mat::Search<int,int,mat::SimpleDigraph,mat::AdjacencyList,mat::StackDFS> s(g,1);
-	std::cout<<s.distanceTo(3);
+	mat::Search<int,int,mat::SimpleDigraph,mat::AdjacencyList,mat::QueueBFS> s(g,1);
+	std::cout<<s.distanceTo(3)<<std::endl;
+	auto p=s.getPath(3);
+	for(auto x:p)
+		std::cout<<x<<' ';
 }
